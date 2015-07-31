ed409529366ae66a99f141503505e4bece18d4b4
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2015-07-31 13:03:51 +0800

    Refactor bindToDispatcher()

--- test/standard_test.js
+++ test/standard_test.js
@@ -1,5 +1,6 @@
 /* global describe, it */
 describe('coding style', function () {
+  this.timeout(5000)
   it('conforms to standard', require('mocha-standard').files([
     'lib/index.js',
     'test/**/*.js'
